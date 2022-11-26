0ce590056a77331f54712711d847189e2b4114bd
KIM <alskfl7777@naver.com>
2022-11-26 15:31:06 +0900

    nohbmin

---
+++ nohbmin/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace nohbmin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
